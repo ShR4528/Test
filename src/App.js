135a44a3fe67b956813685bce416c5e8ec046e18
ShR4528 <107538948+ShR4528@users.noreply.github.com>
2022-09-19 18:08:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import bg from "./img/bg.jpg";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={bg}  alt="no logo" />
       </header>
     </div>
   );
