3bc8b8edfa2f0124b2c45abe9df5c94dc00c0d52
ShR4528 <107538948+ShR4528@users.noreply.github.com>
2022-09-29 13:37:44 +0300

    test

--- src/App.js
+++ src/App.js
@@ -10,8 +10,22 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <img src={bg}  alt="no logo" />
+        <img src={bg} alt="no logo" />
       </header>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        expedita ex quasi consequuntur magni minima nobis temporibus assumenda
+        eaque, voluptas explicabo pariatur officia ad repellat, eius, dolorem
+        cumque fuga reiciendis. Modi laudantium numquam ipsa sunt perferendis
+        necessitatibus optio repudiandae doloribus atque doloremque deleniti
+        alias error incidunt itaque quia, velit blanditiis accusantium? Maxime
+        nesciunt esse itaque impedit iusto perspiciatis asperiores? Voluptatum
+        reiciendis odio officiis enim eius? Animi autem ratione modi, ad vitae
+        sapiente numquam non dolorum suscipit quam aliquam sint aut culpa at
+        provident impedit eum aliquid accusamus eligendi deserunt ab, voluptatem
+        quas, dolores natus. Mollitia beatae minima eligendi doloribus
+        consectetur.
+      </p>
     </div>
   );
 }
